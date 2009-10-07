24e0ce7ed782e381ed5a24a2f6fa4cb8b2720650
dmishd <dmishd@03d07454-6ff5-11de-8585-3f51e31374b7>
2009-10-07 14:55:55 +0000

    email address in contact.php is now set in config

--- httpdocs/contact.php
+++ httpdocs/contact.php
@@ -5,7 +5,7 @@ topbit(6);
 
 <h2>Contact <?php print $guide_name; ?></h2>
 
-<p>If you want to get involved in <?php print $guide_name; ?>, have some feedback or just want to drop us a line, email <strong>info [at] justliving [dot] org [dot] uk</strong></p>
+<p>If you want to get involved in <?php print $guide_name; ?>, have some feedback or just want to drop us a line, email <strong><?php print $contact_email_obfusticated; ?></strong></p>
 
 <p><strong>Note:</strong> Please use <a href="<?php print $app_path; ?>guide/submit.php">the correct page for submitting or suggesting listings</a>.</p>
 
