1ca3b4fd48f2a785bfbfb0e7686d8b281da64114
dmishd <dmishd@03d07454-6ff5-11de-8585-3f51e31374b7>
2009-10-19 17:18:39 +0000

    reverted changes to list.php

--- trunk/httpdocs/guide/list.php
+++ trunk/httpdocs/guide/list.php
@@ -59,12 +59,10 @@ if ($myrow = mysql_fetch_array($result))
 	{
 	do 
 		{
-                  print("<h3 style=\"text-decoration: underline;\">" . 
-                    "<a href=\"view.php?id=" . $myrow["id"] . "&amp;cat=$cat_id\" class=\"block_link listing\">" .
-                    clean_text($myrow["org_name"]) . "</a></h3>\n");
-                  print("<p style=\"color: black; text-decoration: none;\">");
-                  print(create_abstract($myrow["description"]));
-                  print("</p>\n\n");
+                   print("<a href=\"view.php?id=" . $myrow["id"] . "&amp;cat=$cat_id\" class=\"block_link listing\">");
+                   print("<h3 style=\"text-decoration: underline;\">" . clean_text($myrow["org_name"]) . "</h3>\n");
+                   print("<p style=\"color: black; text-decoration: none;\">" . create_abstract($myrow["description"]) . "</p>\n");
+                   print("</a>\n\n");
 		} while ($myrow = mysql_fetch_array($result));
 	}
 	
